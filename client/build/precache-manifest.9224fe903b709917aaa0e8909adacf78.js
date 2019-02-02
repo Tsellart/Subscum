b15f61036925c98a0511f28cb3ef5a13767922a8
TSodo <whigs93@gmail.com>
2019-02-02 09:52:09 -0600

    fix?

---
+++ client/build/precache-manifest.9224fe903b709917aaa0e8909adacf78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aced3b5ec70a4c4011217e262b19b7a7",
+    "url": "/static/media/Plan.aced3b5e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "37fccd8c6fe5f58d0f8b",
+    "url": "/static/js/main.37fccd8c.chunk.js"
+  },
+  {
+    "revision": "08c6793441f24b4248a3",
+    "url": "/static/js/1.08c67934.chunk.js"
+  },
+  {
+    "revision": "37fccd8c6fe5f58d0f8b",
+    "url": "/static/css/main.2bf5798d.chunk.css"
+  },
+  {
+    "revision": "820a7bac4150515ef4922258ea2e73c2",
+    "url": "/index.html"
+  }
+];
